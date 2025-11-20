5b1a157446059bf4c45983f440c0cb5496efbfb5
willy161 <gasper.jezovsek@gmail.com>
2025-11-20 18:22:04 +0100

    maven struktura

---
+++ clani/src/main/java/com/golfapp/clani/ClaniApplication.java
@@ -0,0 +1,14 @@
+package com.golfapp.clani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaniApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ClaniApplication.class, args);
+    System.out.println("sdsd");
+  }
+
+}
---
+++ clani/src/test/java/com/golfapp/clani/ClaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golfapp.clani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
